61740edf799066b3eed9915211cb641b80fc84ae
Ken Johnson <dragonkenkj@gmail.com>
2020-01-23 09:32:50 -0800

    Test

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Button, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './CustomNavbar.css';
 
@@ -7,26 +7,22 @@ export default class CustomNavbar extends Component {
     render() {
         return (
             <Navbar className="navbar" collapseOnSelect>
+                
                 <Nav.Item>
                     <Nav.Link className="ken-johnson" href="/" eventKey="1">Ken Johnson</Nav.Link>
                 </Nav.Item>
-                <Navbar.Toggle />
-                <Navbar.Collapse>
-                    <Nav pullRight>
-                        <Nav.Item>
-                            <Nav.Link href="/" eventKey="1">Home</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="/about" eventKey="2">About</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="/projects" eventKey="3">Projects</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="/timeline" eventKey="4">Timeline</Nav.Link>
-                        </Nav.Item>
-                    </Nav>
-                </Navbar.Collapse>
+                <Nav className="mr-auto">
+                    <Navbar.Toggle />
+                    <Link className="nav-buttons-navbar" to="/about" style={{ textDecoration: 'none' }}>About</Link>
+                    <Link className="nav-buttons-navbar" to="/projects" style={{ textDecoration: 'none' }}>Projects</Link> 
+                </Nav>
+                    
+                <a href="https://www.linkedin.com/in/ken-john/" target="_blank">
+                    <Image src="assets/linkedin.png" className="icons-navbar"/>
+                </a>
+                <a href="https://github.com/Kojon74" target="_blank">
+                    <Image src="assets/github.png" className="icons-navbar"/>
+                </a>
             </Navbar>
         )
     }
