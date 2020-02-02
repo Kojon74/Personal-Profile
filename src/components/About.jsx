271c92398cee88658c34be3b69327fdcf85abac4
Ken Johnson <dragonkenkj@gmail.com>
2020-02-01 18:40:25 -0800

    Everything complete but mobile aspect

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,77 @@
 import React, { Component } from 'react'
-import { Container, Col, Image } from 'react-bootstrap';
+import { Container, Col, Image, Carousel } from 'react-bootstrap';
 import './About.css';
 
 export default class About extends Component {
-    render() {
-        return (
-            <div>
-                  <Image src="assets/Yokohama-wallpaper.jpg" fluid className="yokohama-wallpaper" />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Image className="yokohama-wallpaper" src="assets/Yokohama-wallpaper.jpg" fluid />
+        <div className="about-me-container">
+          <div className="about-me">Leading by example</div>
+          <div className="details">On top of being an older brother to my three siblings, I have had many opportunities 
+            throughout my life to be able to influence others with the decisions they make. From being a robotics, cross 
+            country and tennis team captain to friend, I believe that it is my responsibility to pass down my knowledge 
+            to those around me so that they can succeed and learn from the mistakes I made. Being active with sports has 
+            taught me a lot about determination and being able to stay calm and work through problems even in the toughest 
+            situations. I was born and raised in Japan but moved to Canada to pursue a career in software development.
+          </div>
+        </div>
+        <Carousel className="about-carousel" controls={false} indicators={false} pauseOnHover={false}>
+            <Carousel.Item>
+              <img className="about-carousel-images" src="assets/carousel/IMG_9536_Original.JPG"/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="about-carousel-images" src="assets/carousel/IMG_6240_Original.JPG"/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="about-carousel-images" src="assets/carousel/IMG_1718_Original.JPG"/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="about-carousel-images" src="assets/carousel/IMG_4971_Original.JPG"/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="about-carousel-images" src="assets/carousel/IMG_1759_Original.JPG"/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="about-carousel-images" src="assets/carousel/IMG_4764_Original.JPG"/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="about-carousel-images" src="assets/carousel/IMG_1849_Original.JPG"/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="about-carousel-images" src="assets/carousel/IMG_1890_Original.JPG"/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="about-carousel-images" src="assets/carousel/IMG_0028_Original.JPG"/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="about-carousel-images" src="assets/carousel/IMG_6180_Original.JPG"/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="about-carousel-images" src="assets/carousel/IMG_2658_Original.JPG"/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="about-carousel-images" src="assets/carousel/IMG_5496_Original.JPG"/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="about-carousel-images" src="assets/carousel/IMG_1305_Original.JPG"/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="about-carousel-images" src="assets/carousel/IMG_0066_Original.JPG"/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="about-carousel-images" src="assets/carousel/IMG_6257_Original.JPG"/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="about-carousel-images" src="assets/carousel/IMG_8583_Original.JPG"/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="about-carousel-images" src="assets/carousel/IMG_9535_Original.JPG"/>
+            </Carousel.Item>
+          </Carousel>
+            
+      </div>
+    )
+  }
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,36 +1,33 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+//import { Spring } from 'react-spring'
 import { Jumbotron, Container, Row, Col, Image, Button } from 'react-bootstrap';
 import './Home.css'
 
 export default class Home extends Component {
-    render() {
-        return (
-            <div className="bodyb">
-                <div className="left-half">
-                    <Link className="nav-links" to="/about" style={{ textDecoration: 'none' }}>About</Link>
-                    <Link className="nav-links" to="/projects" style={{ textDecoration: 'none' }}>Projects</Link>
-              
-                    <a href="https://www.linkedin.com/in/ken-john/" target="_blank">
-                        <Image src="assets/linkedin.png" className="icons"/>
-                    </a>
-                    <a href="https://github.com/Kojon74" target="_blank">
-                        <Image src="assets/github.png" className="icons"/>
-                    </a>
+  render() {
+    return (
+        
+      <div className="bodyb">
+        <div className="left-half">
+          <Link className="nav-links" to="/about" style={{ textDecoration: 'none' }}>About</Link>
+          <Link className="nav-links" to="/projects" style={{ textDecoration: 'none' }}>Projects</Link>
+    
+          <a href="https://www.linkedin.com/in/ken-john/" target="_blank">
+              <Image src="assets/linkedin.png" className="icons"/>
+          </a>
+          <a href="https://github.com/Kojon74" target="_blank">
+              <Image src="assets/github.png" className="icons"/>
+          </a>
 
-                    <p className="contact">kenjohnson0704@gmail.com</p>
-                    
-                    
-                </div>
-                <div className="right-half">
-                    <Image src="assets/desk-pic.jpg" className="desk-pic" />
-                    <h2 className="name">Ken Johnson</h2>
-                    <p className="intro">Computer Engineering Student - Software Developer</p>
-                    
-                </div>
-            </div>
-            
-            
-        )
-    }
+          <p className="contact">kenjohnson0704@gmail.com</p>
+        </div>
+        <div className="right-half">
+          <Image src="assets/desk-pic.jpg" className="desk-pic" />
+          <h2 className="name">Ken Johnson</h2>
+          <p className="intro">Computer Engineering Student - Software Developer</p>  
+        </div>
+      </div>   
+    )
+  }
 }
