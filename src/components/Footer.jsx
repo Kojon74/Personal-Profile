0794a565da88f5f92ded9cfe2dfc590fe5898d3c
Ken Johnson <dragonkenkj@gmail.com>
2020-02-17 16:18:50 -0500

    Responsive

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './Footer.css'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className="footer-section" id="contact">
+        <div className="e-mail">kenjohnson0704@gmail.com</div>
+        <div className="copyright">Copyright © 2020 Ken Johnson</div>
+      </div>
+    )
+  }
+}
