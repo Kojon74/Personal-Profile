193fb641705768ee4f62911bed105c097382065d
Ken Johnson <dragonkenkj@gmail.com>
2020-02-27 21:14:22 -0800

    Changed to lighter background image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,55 +17,55 @@ export default class About extends Component {
         </div>
         <Carousel className="about-carousel" controls={false} indicators={false} pauseOnHover={false} fade={true} slide={false}>
             <Carousel.Item>
-              <img className="about-carousel-images" src="assets/carousel/IMG_9536_Original.JPG"/>
+              <img className="about-carousel-images" src="assets/Carousel/IMG_9536_Original.JPG" alt="ERROR"/>
             </Carousel.Item>
             <Carousel.Item>
-              <img className="about-carousel-images" src="assets/carousel/IMG_6240_Original.JPG"/>
+              <img className="about-carousel-images" src="assets/Carousel/IMG_6240_Original.JPG" alt="ERROR"/>
             </Carousel.Item>
             <Carousel.Item>
-              <img className="about-carousel-images" src="assets/carousel/IMG_1718_Original.JPG"/>
+              <img className="about-carousel-images" src="assets/Carousel/IMG_1718_Original.JPG" alt="ERROR"/>
             </Carousel.Item>
             <Carousel.Item>
-              <img className="about-carousel-images" src="assets/carousel/IMG_4971_Original.JPG"/>
+              <img className="about-carousel-images" src="assets/Carousel/IMG_4971_Original.JPG" alt="ERROR"/>
             </Carousel.Item>
             <Carousel.Item>
-              <img className="about-carousel-images" src="assets/carousel/IMG_1759_Original.JPG"/>
+              <img className="about-carousel-images" src="assets/Carousel/IMG_1759_Original.JPG" alt="ERROR"/>
             </Carousel.Item>
             <Carousel.Item>
-              <img className="about-carousel-images" src="assets/carousel/IMG_4764_Original.JPG"/>
+              <img className="about-carousel-images" src="assets/Carousel/IMG_4764_Original.JPG" alt="ERROR"/>
             </Carousel.Item>
             <Carousel.Item>
-              <img className="about-carousel-images" src="assets/carousel/IMG_1849_Original.JPG"/>
+              <img className="about-carousel-images" src="assets/Carousel/IMG_1849_Original.JPG" alt="ERROR"/>
             </Carousel.Item>
             <Carousel.Item>
-              <img className="about-carousel-images" src="assets/carousel/IMG_1890_Original.JPG"/>
+              <img className="about-carousel-images" src="assets/Carousel/IMG_1890_Original.JPG" alt="ERROR"/>
             </Carousel.Item>
             <Carousel.Item>
-              <img className="about-carousel-images" src="assets/carousel/IMG_0028_Original.JPG"/>
+              <img className="about-carousel-images" src="assets/Carousel/IMG_0028_Original.JPG" alt="ERROR"/>
             </Carousel.Item>
             <Carousel.Item>
-              <img className="about-carousel-images" src="assets/carousel/IMG_6180_Original.JPG"/>
+              <img className="about-carousel-images" src="assets/Carousel/IMG_6180_Original.JPG" alt="ERROR"/>
             </Carousel.Item>
             <Carousel.Item>
-              <img className="about-carousel-images" src="assets/carousel/IMG_2658_Original.JPG"/>
+              <img className="about-carousel-images" src="assets/Carousel/IMG_2658_Original.JPG" alt="ERROR"/>
             </Carousel.Item>
             <Carousel.Item>
-              <img className="about-carousel-images" src="assets/carousel/IMG_5496_Original.JPG"/>
+              <img className="about-carousel-images" src="assets/Carousel/IMG_5496_Original.JPG" alt="ERROR"/>
             </Carousel.Item>
             <Carousel.Item>
-              <img className="about-carousel-images" src="assets/carousel/IMG_1305_Original.JPG"/>
+              <img className="about-carousel-images" src="assets/Carousel/IMG_1305_Original.JPG" alt="ERROR"/>
             </Carousel.Item>
             <Carousel.Item>
-              <img className="about-carousel-images" src="assets/carousel/IMG_0066_Original.JPG"/>
+              <img className="about-carousel-images" src="assets/Carousel/IMG_0066_Original.JPG" alt="ERROR"/>
             </Carousel.Item>
             <Carousel.Item>
-              <img className="about-carousel-images" src="assets/carousel/IMG_6257_Original.JPG"/>
+              <img className="about-carousel-images" src="assets/Carousel/IMG_6257_Original.JPG" alt="ERROR"/>
             </Carousel.Item>
             <Carousel.Item>
-              <img className="about-carousel-images" src="assets/carousel/IMG_8583_Original.JPG"/>
+              <img className="about-carousel-images" src="assets/Carousel/IMG_8583_Original.JPG" alt="ERROR"/>
             </Carousel.Item>
             <Carousel.Item>
-              <img className="about-carousel-images" src="assets/carousel/IMG_9535_Original.JPG"/>
+              <img className="about-carousel-images" src="assets/Carousel/IMG_9535_Original.JPG" alt="ERROR"/>
             </Carousel.Item>
           </Carousel>
             
